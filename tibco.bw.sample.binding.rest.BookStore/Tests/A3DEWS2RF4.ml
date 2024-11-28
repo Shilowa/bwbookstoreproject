7451d4569a336bc830ad4b9a1bef2057122223c1
Shilowa <shilowaliver@gmail.com>
2024-11-28 14:03:02 +0200

    Add files via upload

---
+++ tibco.bw.sample.binding.rest.BookStore/Tests/A3DEWS2RF4.ml
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="ASCII"?>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <CurrentLoadedTest href="tibco.bw.sample.binding.rest.BookStore1.bwt#/"/>
+  <EmulationDataFile>ACED00057400595C746962636F2E62772E73616D706C652E62696E64696E672E726573742E426F6F6B53746F72655C54657374735C746962636F2E62772E73616D706C652E62696E64696E672E726573742E426F6F6B53746F7265312E627774</EmulationDataFile>
+</emulation:BWTFile>
